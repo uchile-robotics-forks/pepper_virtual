e43e1c3f84bb0ce40afe1b215cf4f60a8ef78e45
Suwen Leong <leongsuwen@yahoo.com>
2019-01-08 18:46:44 +1100

    working ish

--- pepper_gazebo_plugin/scripts/test_laser_scan_merger.py
+++ pepper_gazebo_plugin/scripts/test_laser_scan_merger.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python
+
+from math import atan2, cos, sin, sqrt
+
 import rospy
-from message_filters import TimeSynchronizer, Subscriber
-from math import sqrt, cos, sin
-from sensor_msgs.msg import LaserScan, PointCloud2
 from tf import TransformListener
-from geometry_msgs.msg import PointStamped
 from laser_geometry import LaserProjection
+from sensor_msgs.msg import LaserScan, PointCloud2
+from message_filters import TimeSynchronizer, Subscriber
 from sensor_msgs.point_cloud2 import read_points, create_cloud_xyz32
 
 
@@ -29,13 +31,12 @@ class LaserMerger(object):
             '/pepper/pc_right', PointCloud2, queue_size=1)
         self.cloud_pub = rospy.Publisher(
             'cloud_in', PointCloud2, queue_size=1)
+        self.laser_pub = rospy.Publisher(
+            '/pepper/laser_scan', LaserScan, queue_size=1)
 
         self.ts = TimeSynchronizer(
             [left_sub, front_sub, right_sub], 10)
         self.ts.registerCallback(self.scan_cb)
-
-        # For debugging
-        self.a = 0
 
     def get_dist(self, x0, y0, x1=0, y1=0):
         return sqrt((x1 - x0)**2 + (y1 - y0)**2)
@@ -49,16 +50,11 @@ class LaserMerger(object):
             print "Failed to transform laser scan because: " + str(e)
 
         # For debugging
-        nontranslated_points = []
+        # nontranslated_points = []
         translated_points = []
 
-        nontranslated_points.append("Data set " + str(self.a))
-        self.a += 1
-
         # left pc translation
-        nontranslated_points.append("Left")
-        for p in read_points(pc_left, skip_nans=True):
-            nontranslated_points.append(p)
+        for p in read_points(pc_left, field_names=('x', 'y', 'z'), skip_nans=True):
             x = p[0]
             y = p[1]
             newx = x * cos(1.757) - y * sin(1.757)  # - 0.102
@@ -66,17 +62,13 @@ class LaserMerger(object):
             point = (newx, newy, 0.0)
             translated_points.append(point)
 
-        nontranslated_points.append("Front")
         # front point cloud
-        for p in read_points(pc_front, skip_nans=True):
-            nontranslated_points.append(p)
+        for p in read_points(pc_front, field_names=('x', 'y', 'z'), skip_nans=True):
             point = (p[0], p[1], p[2])
             translated_points.append(point)
 
         # right point cloud translation
-        nontranslated_points.append("Right")
-        for p in read_points(pc_right, skip_nans=True):
-            nontranslated_points.append(p)
+        for p in read_points(pc_right, field_names=('x', 'y', 'z'), skip_nans=True):
             x = p[0]
             y = p[1]
             newx = x * cos(-1.757) - y * sin(-1.757)  # - 0.102
@@ -84,14 +76,13 @@ class LaserMerger(object):
             point = (newx, newy, 0.0)
             translated_points.append(point)
 
-        print translated_points
         point_cloud = create_cloud_xyz32(pc_front.header, translated_points)
 
+        # Publish all point clouds
         self.left_pub.publish(pc_left)
         self.front_pub.publish(pc_front)
         self.right_pub.publish(pc_right)
         self.cloud_pub.publish(point_cloud)
-
 
 if __name__ == "__main__":
     lm = LaserMerger()
